26fded78ed2ea8839763abc760601633f69b5e88
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-10-08 21:13:20 +0200

    Add websocket communication

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {useEffect} from "react";
+import shortid from "shortid"
+import io from 'socket.io-client';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState([]);
+    const [taskName, setTaskName] = useState('');
+    const [socket, setSocket] = useState('');
+    useEffect(() => {
+        const socket = io('http://localhost:8000');
+        socket.on('updateData', (serverTasks) => {setTasks(serverTasks)})
+        socket.on('addTask', (serverTask)=>{addTask(serverTask)})
+        socket.on('removeTask', (id)=>{removeTask(id, false)})
+        setSocket(socket);
+    }, []);
+    const addTask = (task) => {
+        console.log('Add')
+        setTasks(tasks => [...tasks, task])
+        socket.emit('addTask', task)
+    }
+    const removeTask = (id, send) => {
+        setTasks(tasks => tasks.filter(task => task.id !== id))
+        if (send){socket.emit('removeTask', id)}
+    }
+    const submitForm = (e) => {
+        e.preventDefault()
+        addTask({id: shortid(), name: taskName})
+    }
+    return (
+        <div className="App">
+            <header>
+                <h1>ToDoList.app</h1>
+            </header>
+            <section className="tasks-section" id="tasks-section">
+                <h2>Tasks</h2>
+                <ul className="tasks-section__list" id="tasks-list">
+                    {tasks.map((task)=>{return(<li key={task.id} className="task">{task.name}<button onClick={()=>removeTask(task.id, true)} className="btn btn--red">Remove</button></li>)})}
+                </ul>
+                <form id="add-task-form" onSubmit={submitForm}>
+                    <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" value={taskName} onChange={(e)=>setTaskName(e.target.value)}/>
+                    <button className="btn" type="submit">Add</button>
+                </form>
+            </section>
+        </div>
+    );
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const path = require('path')
+const socket = require('socket.io');
 const app = express()
 const port = 8000
 
+// Data
+let tasks = [{id: 0, name: 'walk the dog'}];
+// Express server
 app.use(express.static(path.join(__dirname,'client/build')))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'client/build/index.html'))
 })
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+const server = app.listen(port, () => {
+    console.log(`to-do app listening on port ${port}`)
 })
+// Websocket communication
+const io = socket(server, {
+    cors: {
+        origin: '*',
+    }
+});
+io.on('connection', (socket) => {
+    socket.emit('updateData', tasks)
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('removeTask', (id) => {
+        tasks = tasks.filter((task) => {return task.id !== id})
+        socket.broadcast.emit('removeTask', id)
+    })
+    socket.on('addTask', (task) => {
+        tasks.push(task);
+        socket.broadcast.emit('addTask', task)
+    })
+});
